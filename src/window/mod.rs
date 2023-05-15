3360bee057f798e68484315c27ec35d849fffdaa
Ava Drumm <lmbdrumm@gmail.com>
2023-05-15 14:27:41 +0200

    Render function now renders title to look like a title and added support for author and license

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -1,10 +1,11 @@
 mod imp;
 
 use crate::athn_document::AthnDocument;
+use adw::prelude::*;
 use adw::subclass::prelude::*;
+use adw::Application;
 use glib::Object;
 use gtk::{gio, glib, Label};
-use adw::Application;
 
 glib::wrapper! {
     pub struct Window(ObjectSubclass<imp::Window>)
@@ -20,7 +21,62 @@ impl Window {
     }
 
     pub fn render(&self, document: AthnDocument) {
-        let title = Label::new(Some(document.metadata.title.as_str()));
+        // Show title metadata attribute
+        let title = Label::builder()
+            .label(document.metadata.title.as_str())
+            .halign(gtk::Align::Start)
+            .build();
+        title.add_css_class("large-title");
         self.imp().canvas.append(&title);
+
+        // Show author and license metadata attribute
+        let author_formatter = |acc: String, val: &String| {
+            if acc.is_empty() {
+                val.to_string()
+            } else {
+                format!("{acc}, {val}")
+            }
+        };
+        let license_formatter = |acc: String, val: &String| {
+            if acc.is_empty() {
+                val.to_string()
+            } else {
+                format!("{acc}, {val}")
+            }
+        };
+        match (document.metadata.author, document.metadata.license) {
+            (Some(author), Some(license)) => {
+                let metaline = Label::builder()
+                    .label(format!(
+                        "By: {}. License: {}",
+                        author.iter().fold(String::new(), author_formatter),
+                        license.iter().fold(String::new(), license_formatter)
+                    ))
+                    .halign(gtk::Align::Start)
+                    .build();
+                self.imp().canvas.append(&metaline);
+            }
+            (Some(author), None) => {
+                let metaline = Label::builder()
+                    .label(format!(
+                        "By: {}",
+                        author.iter().fold(String::new(), author_formatter)
+                    ))
+                    .halign(gtk::Align::Start)
+                    .build();
+                self.imp().canvas.append(&metaline);
+            }
+            (None, Some(license)) => {
+                let metaline = Label::builder()
+                    .label(format!(
+                        "License: {}",
+                        license.iter().fold(String::new(), license_formatter)
+                    ))
+                    .halign(gtk::Align::Start)
+                    .build();
+                self.imp().canvas.append(&metaline);
+            }
+            (None, None) => (),
+        }
     }
 }
