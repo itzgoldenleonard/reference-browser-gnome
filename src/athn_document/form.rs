b7b37e30d671923bade3f8cd06ee9f5cc931a328
Ava Drumm <lmbdrumm@gmail.com>
2023-06-01 14:59:29 +0200

    Added string form field type to the parser

--- src/athn_document/form.rs
+++ src/athn_document/form.rs
@@ -1,7 +1,7 @@
 #[derive(PartialEq, Debug)]
 pub enum FormField {
-    Submit(SubmitField),
-    String(StringField),
+    Submit(ID, SubmitField),
+    String(ID, StringField),
 }
 
 // Helper structs
@@ -37,22 +37,20 @@ impl ID {
 #[derive(PartialEq, Debug)]
 pub struct ConditionalProperty {
     pub inverse: bool, // inverse == true is a conditional not
-    pub id: ID,
+    pub target: ID,
 }
 
 #[derive(PartialEq, Debug)]
 pub struct GlobalProperties<T> {
-    pub id: ID,
     pub optional: bool,
-    pub label: String,
-    pub default: T,
-    pub conditional: ConditionalProperty,
+    pub label: Option<String>,
+    pub default: Option<T>,
+    pub conditional: Option<ConditionalProperty>,
 }
 
 // Field type structs
 #[derive(PartialEq, Debug)]
 pub struct SubmitField {
-    pub id: ID,
     pub dest: String, // Like with Link this isnt parsed as a URL yet because it can be relative
     pub label: Option<String>,
     pub redirect: bool,
@@ -61,8 +59,8 @@ pub struct SubmitField {
 #[derive(PartialEq, Debug)]
 pub struct StringField {
     pub global: GlobalProperties<String>,
-    pub min: u32,
-    pub max: u32,
+    pub min: Option<u32>,
+    pub max: Option<u32>,
     pub multiline: bool,
     pub secret: bool,
     pub allowed_variants: Option<Vec<String>>,
@@ -72,34 +70,121 @@ impl FormField {
     pub fn parse(input: &str) -> Result<FormField, &str> {
         use FormField::*;
 
+        // Turn the &str into a ID, type and properties variables that I can work with
         let (id, input) = input.split_once(":").ok_or("Form field with no ID found")?;
+        let id = ID::new(id)?;
         let (field_type, input) = input.split_once(" \\").unwrap_or((input, ""));
         let properties: Vec<(&str, &str)> = input
             .split(" \\")
             .map(|property| property.split_once(" ").unwrap_or((property, "")))
             .collect();
 
+        // Create an appropriate FormField based on the type found
         match field_type {
-            "submit" => Ok(Submit(SubmitField {
-                id: ID::new(id)?,
+            "submit" => Ok(Submit(
+                id,
+                SubmitField {
+                    dest: properties
+                        .iter()
+                        .find(|e| e.0 == "dest")
+                        .ok_or("Submit type form field without destination found")?
+                        .1
+                        .to_string(),
 
-                dest: properties
-                    .iter()
-                    .find(|e| e.0 == "dest")
-                    .ok_or("Submit type form field without destination found")?
-                    .1
-                    .to_string(),
+                    label: properties
+                        .iter()
+                        .find(|e| e.0 == "label" || e.0 == "l")
+                        .map(|e| e.1.to_string()),
 
-                label: properties
-                    .iter()
-                    .find(|e| e.0 == "label" || e.0 == "l")
-                    .map(|e| e.1.to_string()),
+                    redirect: properties.iter().find(|e| e.0 == "redirect").is_some(),
+                },
+            )),
+            "string" => Ok(String(
+                id,
+                StringField {
+                    global: GlobalProperties::<std::string::String>::parse(&properties, |s| {
+                        s.to_string()
+                    })?,
 
-                redirect: properties.iter().find(|e| e.0 == "redirect").is_some(),
-            })),
+                    min: find_number_property(
+                        &properties,
+                        "min",
+                        "String type form field with anvalid min property value found",
+                    )?,
+
+                    max: find_number_property(
+                        &properties,
+                        "max",
+                        "String type form field with anvalid max property value found",
+                    )?,
+
+                    multiline: properties.iter().find(|e| e.0 == "multiline").is_some(), 
+                    // TODO: Maybe it would be a good idea to have a function for this
+                    secret: properties.iter().find(|e| e.0 == "secret").is_some(),
+
+                    allowed_variants: match properties.iter().find(|e| e.0 == "e") {
+                        Some(_) => Some(
+                            properties
+                                .iter()
+                                .filter(|e| e.0 == "e")
+                                .map(|e| e.1.to_string())
+                                .collect(),
+                        ),
+                        None => None,
+                    },
+                },
+            )),
             _ => Err("Form field with invalid type found"),
         }
     }
+}
+
+impl<U> GlobalProperties<U> {
+    pub fn parse<T>(
+        input: &Vec<(&str, &str)>,
+        converter: fn(&str) -> T,
+    ) -> Result<GlobalProperties<T>, &'static str> {
+        Ok(GlobalProperties {
+            optional: input
+                .iter()
+                .find(|e| e.0 == "optional" || e.0 == "?")
+                .is_some(),
+
+            label: input
+                .iter()
+                .find(|e| e.0 == "label" || e.0 == "l")
+                .map(|e| e.1.to_string()),
+
+            default: input
+                .iter()
+                .find(|e| e.0 == "default" || e.0 == "d")
+                .map(|e| converter(e.1)),
+
+            conditional: input
+                .iter()
+                .find(|e| e.0 == "c" || e.0 == "!c")
+                .map(|e| {
+                    Ok::<ConditionalProperty, &str>(ConditionalProperty {
+                        inverse: e.0.starts_with("!"),
+                        target: ID::new(e.1).map_err(|_| "Invalid ID")?,
+                    })
+                })
+                .transpose()?,
+        })
+    }
+}
+
+fn find_number_property<'a, F: std::str::FromStr>(
+    input: &Vec<(&str, &str)>,
+    property_name: &str,
+    err: &'a str,
+) -> Result<Option<F>, &'a str> {
+    input
+        .iter()
+        .find(|e| e.0 == property_name)
+        .map(|e| e.1.parse())
+        .transpose()
+        .map_err(|_| err)
 }
 
 mod tests {
@@ -118,7 +203,90 @@ mod tests {
     }
 
     #[test]
-    fn basic_string_field() {}
+    fn basic_string_field() {
+        let expected = FormField::String(
+            ID::new("string").unwrap(),
+            StringField {
+                global: GlobalProperties {
+                    optional: false,
+                    label: Some("This is a test".to_string()),
+                    default: None,
+                    conditional: None,
+                },
+                min: None,
+                max: None,
+                multiline: false,
+                secret: false,
+                allowed_variants: None,
+            },
+        );
+
+        let line = "string:string \\l This is a test";
+
+        let form = FormField::parse(line).unwrap();
+
+        assert_eq!(form, expected);
+    }
+
+    #[test]
+    fn advanced_string_field() {
+        let expected = FormField::String(
+            ID::new("string").unwrap(),
+            StringField {
+                global: GlobalProperties {
+                    optional: false,
+                    label: Some("This is a test".to_string()),
+                    default: Some("Fill me out".to_string()),
+                    conditional: Some(ConditionalProperty {
+                        inverse: true,
+                        target: ID::new("other_id").unwrap(),
+                    }),
+                },
+                min: Some(10),
+                max: Some(500),
+                multiline: true,
+                secret: false,
+                allowed_variants: None,
+            },
+        );
+
+        let line =
+            "string:string \\l This is a test \\multiline \\min 10 \\max 500 \\d Fill me out \\!c other_id";
+
+        let form = FormField::parse(line).unwrap();
+
+        assert_eq!(form, expected);
+    }
+
+    #[test]
+    fn enum_string_field() {
+        let expected = FormField::String(
+            ID::new("string").unwrap(),
+            StringField {
+                global: GlobalProperties {
+                    optional: false,
+                    label: Some("This is a test".to_string()),
+                    default: None,
+                    conditional: None,
+                },
+                min: None,
+                max: None,
+                multiline: false,
+                secret: false,
+                allowed_variants: Some(vec![
+                    "Variant 1".to_string(),
+                    "Variant 2".to_string(),
+                    "Variant 3".to_string(),
+                ]),
+            },
+        );
+
+        let line = "string:string \\l This is a test \\e Variant 1 \\e Variant 2 \\e Variant 3";
+
+        let form = FormField::parse(line).unwrap();
+
+        assert_eq!(form, expected);
+    }
 
     #[test]
     fn label_shorthand() {
@@ -133,12 +301,14 @@ mod tests {
 
     #[test]
     fn submit_field_with_dest() {
-        let expected = FormField::Submit(SubmitField {
-            id: ID::new("Send").unwrap(),
-            dest: "/destination".to_string(),
-            label: None,
-            redirect: false,
-        });
+        let expected = FormField::Submit(
+            ID::new("Send").unwrap(),
+            SubmitField {
+                dest: "/destination".to_string(),
+                label: None,
+                redirect: false,
+            },
+        );
 
         let line = "Send:submit \\dest /destination";
 
@@ -149,12 +319,14 @@ mod tests {
 
     #[test]
     fn submit_field_with_redirect() {
-        let expected = FormField::Submit(SubmitField {
-            id: ID::new("Send").unwrap(),
-            dest: "/destination".to_string(),
-            label: None,
-            redirect: true,
-        });
+        let expected = FormField::Submit(
+            ID::new("Send").unwrap(),
+            SubmitField {
+                dest: "/destination".to_string(),
+                label: None,
+                redirect: true,
+            },
+        );
 
         let line = "Send:submit \\dest /destination \\redirect";
 
@@ -165,12 +337,14 @@ mod tests {
 
     #[test]
     fn submit_field_with_redirect_and_label() {
-        let expected = FormField::Submit(SubmitField {
-            id: ID::new("Send").unwrap(),
-            dest: "/destination".to_string(),
-            label: Some("Click here to submit".to_string()),
-            redirect: true,
-        });
+        let expected = FormField::Submit(
+            ID::new("Send").unwrap(),
+            SubmitField {
+                dest: "/destination".to_string(),
+                label: Some("Click here to submit".to_string()),
+                redirect: true,
+            },
+        );
 
         let line = "Send:submit \\dest /destination \\redirect \\label Click here to submit";
 
